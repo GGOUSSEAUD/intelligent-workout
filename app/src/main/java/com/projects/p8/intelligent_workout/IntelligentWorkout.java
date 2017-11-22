ddf82f55bfb99467fccfec5271795f65a298ca69
rapiiidooo <vincent93400@hotmail.fr>
2017-11-22 23:31:33 +0100

    - All mouvement fixed - menu picture added - test for many lvl done - preview added - check if win added

--- app/src/main/java/com/projects/p8/intelligent_workout/IntelligentWorkout.java
+++ app/src/main/java/com/projects/p8/intelligent_workout/IntelligentWorkout.java
@@ -22,6 +22,14 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
     private Bitmap      redblock;
     private Bitmap 		blueblock;
     private Bitmap 		greenblock;
+    private Bitmap      tredblockprev;
+    private Bitmap 		tblueblockprev;
+    private Bitmap 		tgreenblockprev;
+    private Bitmap      redblockprev;
+    private Bitmap 		blueblockprev;
+    private Bitmap 		greenblockprev;
+    private Bitmap 		tmenu;
+    private Bitmap 		menu;
 
     // Declaration des objets Ressources et Context permettant d'accéder aux ressources de notre application et de les charger
     private Resources 	mRes;
@@ -29,8 +37,9 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
 
     // tableau modelisant la carte du jeu
     int[][] carte;
+    int[][] carteprev;
 
-
+    int lvl = 0;
     // constante modelisant les differentes types de cases
     static final int    CST_redblock    = 0;
     static final int    CST_blueblock   = 1;
@@ -39,6 +48,9 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
     // ancres pour pouvoir centrer la carte du jeu
     int        carteTopAnchor;                   // coordonnées en Y du point d'ancrage de notre carte
     int        carteLeftAnchor;                  // coordonnées en X du point d'ancrage de notre carte
+    int        cartePrevTopAnchor;
+    int        cartePrevLeftAnchor;
+
 
     static double screenX;
     static double screenY;
@@ -48,6 +60,8 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
     static final int    carteHeight   = 5;
     static int          carteTileWidth = 0;
     static int          carteTileHeight = 0;
+    static int          cartePrevTileWidth = 0;
+    static int          cartePrevTileHeight = 0;
 
     // indice courant du touchEvent
     static int iX = -1;
@@ -66,9 +80,33 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
     int [][] ref    = {
             {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
             {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
-            {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
+            {CST_redblock, CST_redblock, CST_redblock, CST_redblock, CST_redblock},
             {CST_blueblock, CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock},
             {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock}
+    };
+
+    int [][] refprev    = {
+            {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
+            {CST_redblock, CST_redblock, CST_redblock, CST_redblock, CST_redblock},
+            {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_redblock, CST_blueblock, CST_blueblock}
+    };
+
+    int [][] ref2    = {
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_greenblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock}
+    };
+
+    int [][] refprev2    = {
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_greenblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock},
+            {CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock, CST_blueblock}
     };
 
     private     boolean in = true;
@@ -107,12 +145,21 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
                     for (int j=0; j< carteWidth; j++)
                     {
                         carte[j][i]= ref[j][i];
+                        carteprev[j][i]= refprev[j][i];
                     }
                 }
                 break;
             }
             case 1:
             {
+                for (int i=0; i< carteHeight; i++)
+                {
+                    for (int j=0; j< carteWidth; j++)
+                    {
+                        carte[j][i]= ref2[j][i];
+                        carteprev[j][i]= refprev2[j][i];
+                    }
+                }
                 break;
             }
             case 2:
@@ -132,17 +179,34 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
         screenX = getWidth();
         screenY = getHeight();
 
-        carteTopAnchor = getHeight() / 3;
-        carteLeftAnchor = 0; // (getWidth()- carteWidth*carteTileSize)/2;
+        carteTopAnchor = (int)screenY / 3;
+        carteLeftAnchor = 0;
         carteTileHeight = (getHeight()-carteTopAnchor) / 5 ;
         carteTileWidth  = getWidth() / 5;
 
+        cartePrevTopAnchor = (int) screenY/20;
+        cartePrevLeftAnchor = (int) screenX/4;
+        cartePrevTileWidth = ((int) (screenX) - 2*cartePrevLeftAnchor) / carteWidth;
+        cartePrevTileHeight = (carteTopAnchor - 2*cartePrevTopAnchor) / carteHeight;
+
+        //image pour le main board
         tredblock       = BitmapFactory.decodeResource(mRes, R.drawable.redblock);
         tblueblock      = BitmapFactory.decodeResource(mRes, R.drawable.blueblock);
         tgreenblock     = BitmapFactory.decodeResource(mRes, R.drawable.greenblock);
         redblock        = Bitmap.createScaledBitmap(tredblock, carteTileWidth, carteTileHeight, true);
         blueblock 		= Bitmap.createScaledBitmap(tblueblock, carteTileWidth, carteTileHeight, true);
         greenblock 		= Bitmap.createScaledBitmap(tgreenblock, carteTileWidth, carteTileHeight, true);
+
+        //image pour la preview
+        tredblockprev   = BitmapFactory.decodeResource(mRes, R.drawable.redblock);
+        tblueblockprev  = BitmapFactory.decodeResource(mRes, R.drawable.blueblock);
+        tgreenblockprev = BitmapFactory.decodeResource(mRes, R.drawable.greenblock);
+        redblockprev    = Bitmap.createScaledBitmap(tredblockprev, cartePrevTileWidth, cartePrevTileHeight, true);
+        blueblockprev 	= Bitmap.createScaledBitmap(tblueblockprev, cartePrevTileWidth, cartePrevTileHeight, true);
+        greenblockprev 	= Bitmap.createScaledBitmap(tgreenblockprev, cartePrevTileWidth, cartePrevTileHeight, true);
+
+        tmenu   = BitmapFactory.decodeResource(mRes, R.drawable.menu);
+        menu    = Bitmap.createScaledBitmap(tmenu, cartePrevLeftAnchor/2, (int) (cartePrevTileHeight*1.5), true);
 
         paint = new Paint();
         paint.setColor(0xff0000);
@@ -155,8 +219,8 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
         paint.setStrokeWidth(3);
         paint.setTextAlign(Paint.Align.LEFT);
         carte           = new int[carteHeight][carteWidth];
-
-        loadlevel(0);
+        carteprev       = new int[carteHeight][carteWidth];
+        loadlevel(lvl);
 
         if ((cv_thread!=null) && (!cv_thread.isAlive())) {
             cv_thread.start();
@@ -187,6 +251,33 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
         }
     }
 
+    private void paintpreview(Canvas canvas)
+    {
+        for (int i=0; i< carteHeight; i++)
+        {
+            for (int j=0; j< carteWidth; j++)
+            {
+                switch (carteprev[i][j])
+                {
+                    case CST_redblock:
+                        canvas.drawBitmap(redblockprev, cartePrevLeftAnchor+ j*cartePrevTileWidth, cartePrevTopAnchor+ i*cartePrevTileHeight, null);
+                        break;
+                    case CST_blueblock:
+                        canvas.drawBitmap(blueblockprev, cartePrevLeftAnchor+ j*cartePrevTileWidth, cartePrevTopAnchor+ i*cartePrevTileHeight, null);
+                        break;
+                    case CST_greenblock:
+                        canvas.drawBitmap(greenblockprev, cartePrevLeftAnchor+ j*cartePrevTileWidth, cartePrevTopAnchor+ i*cartePrevTileHeight, null);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void paintmenu(Canvas canvas)
+    {
+        canvas.drawBitmap(menu, cartePrevLeftAnchor/5, cartePrevTopAnchor, null);
+    }
+
     private void decaleCarte (int ind, int num)
     {
         int tmp, i, j;
@@ -204,7 +295,7 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
             case 2:
                 //decale a gauche
                 tmp = carte[ind][0];
-                for (j = 0; j < carteWidth; ++j)
+                for (j = 0; j < carteWidth-1; ++j)
                 {
                     carte[ind][j] = carte[ind][j+1];
                 }
@@ -213,7 +304,7 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
             case 3:
                 //decale en haut
                 tmp = carte[0][ind];
-                for (i = 0; i < carteHeight; ++i)
+                for (i = 0; i < carteHeight-1; ++i)
                 {
                     carte[i][ind] = carte[i+1][ind];
                 }
@@ -231,140 +322,68 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
         }
     }
 
-    // dessin de la carte du jeu
-    private void updatexpaintcarte(Canvas canvas)
-    {
-        for (int i=0; i< carteHeight; i++)
-        {
-            for (int j=0; j< carteWidth; j++)
-            {
-                if(i == iX)
-                {
-                    switch (carte[i][j])
-                    {
-                        case CST_redblock:
-                            canvas.drawBitmap(redblock, carteLeftAnchor + j * carteTileWidth + (int)touchX, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_blueblock:
-                            int val = IsOut(carteLeftAnchor+ j*carteTileWidth + (int)touchX, 0);
-                            if(val == 0) //n'est pas out
-                                canvas.drawBitmap(blueblock, carteLeftAnchor+ j*carteTileWidth + (int)touchX, carteTopAnchor+ i*carteTileHeight, null);
-                            else if (val == 1) //out a droite
-                            {
-                                canvas.drawBitmap(blueblock, carteLeftAnchor + (int)touchX, carteTopAnchor + i * carteTileHeight, null);
-                            }
-                            else if (val == 2)//out a gauche
-                                canvas.drawBitmap(blueblock, carteWidth*carteTileWidth + (int)touchX, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_greenblock:
-                            canvas.drawBitmap(greenblock, carteLeftAnchor+ j*carteTileWidth + (int)touchX, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                    }
-                }
-                else
-                {
-                    switch (carte[i][j])
-                    {
-                        case CST_redblock:
-                            canvas.drawBitmap(redblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_blueblock:
-                            canvas.drawBitmap(blueblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_greenblock:
-                            canvas.drawBitmap(greenblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                    }
-                }
-            }
-        }
-    }
-
-    private void updateypaintcarte(Canvas canvas)
-    {
-        for (int i=0; i< carteHeight; i++)
-        {
-            for (int j=0; j< carteWidth; j++)
-            {
-                if (j == iY)
-                {
-                    switch (carte[i][j])
-                    {
-                        case CST_redblock:
-                            canvas.drawBitmap(redblock, carteLeftAnchor + j * carteTileWidth, carteTopAnchor+ i*carteTileHeight + (int)touchY, null);
-                            break;
-                        case CST_blueblock:
-                            canvas.drawBitmap(blueblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight + (int)touchY, null);
-                            break;
-                        case CST_greenblock:
-                            canvas.drawBitmap(greenblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight + (int)touchY, null);
-                            break;
-                    }
-                }
-                else
-                {
-                    switch (carte[i][j])
-                    {
-                        case CST_redblock:
-                            canvas.drawBitmap(redblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_blueblock:
-                            canvas.drawBitmap(blueblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                        case CST_greenblock:
-                            canvas.drawBitmap(greenblock, carteLeftAnchor+ j*carteTileWidth, carteTopAnchor+ i*carteTileHeight, null);
-                            break;
-                    }
-                }
-            }
-        }
-    }
-
     // permet d'identifier si la partie est gagnee (tous les diamants à leur place)
     private boolean isWon()
     {
+        for (int i=0; i< carteHeight; i++)
+        {
+            for (int j=0; j< carteWidth; j++)
+            {
+                if (carte[j][i] != refprev[j][i])
+                    return false;
+            }
+        }
         return true;
     }
 
     // dessin du jeu (fond uni, en fonction du jeu gagne ou pas dessin du plateau)
     private void nDraw(Canvas canvas)
     {
+        if(isWon())
+        {
+            lvl++;
+            loadlevel(lvl);
+        }
         if(lock_row)
         {
             canvas.drawRGB(44,44,44);
 
             if(lock_rowx)
             {
-                if(Math.abs(touchX) > carteTileWidth && touchX > 0)
+                if(Math.abs(touchX) > carteTileWidth/2 && touchX > 0)
                 {
                     decaleCarte(iX, 1);
                     touchDebutX = touchDebutX + touchX;
                 }
-                else if(Math.abs(touchX) > carteTileWidth && touchX < 0)
+                else if(Math.abs(touchX) > carteTileWidth/2 && touchX < 0)
                 {
                     decaleCarte(iX, 2);
-                    touchDebutX = touchDebutX - touchX;
+                    touchDebutX = touchDebutX + touchX;
                 }
             }
             else if(lock_rowy)
             {
-                if(Math.abs(touchY) > carteTileHeight && touchY < 0)
+                if(Math.abs(touchY) > carteTileHeight/2 && touchY < 0)
                 {
                     decaleCarte(iY, 3);
                     touchDebutY = touchDebutY + touchY;
                 }
-                else if(Math.abs(touchY) > carteTileHeight && touchY > 0)
+                else if(Math.abs(touchY) > carteTileHeight/2 && touchY > 0)
                 {
                     decaleCarte(iY, 4);
                     touchDebutY = touchDebutY + touchY;
                 }
             }
             paintcarte(canvas);
+            paintpreview(canvas);
+            paintmenu(canvas);
         }
         else
         {
             canvas.drawRGB(44,44,44);
             paintcarte(canvas);
+            paintpreview(canvas);
+            paintmenu(canvas);
         }
     }
 
@@ -414,34 +433,6 @@ public class IntelligentWorkout extends SurfaceView implements SurfaceHolder.Cal
             }
         }
     }
-
-    private int IsOut(int val, int num)
-    {
-        // num :    0 check on width
-        //          1 check on height
-
-        if (num == 0)
-        {
-            if(val > carteWidth * carteTileWidth)
-                return 1;
-            if(val < carteLeftAnchor)
-                return 2;
-        }
-        if (num == 1)
-        {
-            if(val > carteHeight * carteTileHeight)
-                return 3;
-            if(val < 0)
-                return 4;
-        }
-        return 0;
-    }
-
-    private void modifyCarte ()
-    {
-
-    }
-
 
     // fonction permettant de recuperer les retours clavier
     @Override
